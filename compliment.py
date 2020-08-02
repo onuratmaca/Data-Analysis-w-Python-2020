6dbf9bc9b1004bcc47043e85a478133ed1b8358f
Onur Atmaca <oatm92@gmail.com>
2020-08-02 17:45:23 -0500

    exercise 2

---
+++ compliment.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Exercise 2 (compliment)
+Fill in the stub solution to make the program work as follows.
+ The program should ask the user for an input, and the print an answer as the examples below show.
+
+What country are you from? Sweden
+I have heard that Sweden is a beautiful country.
+
+What country are you from? Chile
+I have heard that Chile is a beautiful country.
+"""
+
+def main():
+    # Enter you solution here
+    country = input("What country are you from? ")
+    print(f"I have heard that {country} is a beautiful country.")
+
+if __name__ == "__main__":
+    main()
